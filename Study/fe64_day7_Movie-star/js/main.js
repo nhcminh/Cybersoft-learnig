ed0d3e37e5c57ec4b7d245fd3d9730d31c880a80
unknown <a2mrken@gmail.com>
2021-04-22 17:03:20 +0700

    update

--- Study/fe64_day7_Movie-star/js/main.js
+++ Study/fe64_day7_Movie-star/js/main.js
@@ -18,37 +18,39 @@ $(document).ready(function() {
     // nav-fix!
     var navbar = document.getElementById('navbar');
     var comingsoon = document.getElementById('incoming');
-    window.addEventListener('scroll', function() {
-            if (window.pageYOffset >= 250) {
-
-
-
+    ['scroll','scrollstart','scrollend'].forEach(function(e){
+        window.addEventListener(e, function() {
+            // if (window.pageYOffset >= 250) {
                 navbar.classList.add('fixed');
                 if (window.pageYOffset >= comingsoon.offsetTop) {
+                    navbar.classList.add('fixed');
                     navbar.style.top = 0;
                     navbar.style.visibility = 'visible';
                     navbar.style.transition = "all 0.5s";
-
-                } else if (window.pageYOffset < comingsoon.offsetTop && window.pageYOffset > 250) {
+                } 
+                else if (window.pageYOffset < comingsoon.offsetTop && window.pageYOffset > 250) {
                     navbar.style.top = "-200px";
                     navbar.style.visibility = "hidden"
                 }
-
-
-
-
-
-            } else {
-
-
+            // } 
+            else {
                 navbar.classList.remove('fixed');
                 navbar.style.removeProperty('top');
                 navbar.style.removeProperty('visibility');
                 navbar.style.removeProperty('transition');
             }
         })
+    });
+    
         // console.log(navbar.offsetTop);
         // console.log(comingsoon.offsetTop);
 
-
+    if (window.innerWidth < 576) {
+        // document.getElementById('header-container').classList.remove('container');
+        // document.getElementById('navbar-container').classList.remove('container');
+        document.getElementById('navbar-container').classList.add('justify-content-between');
+    }else {
+        document.getElementById('header-container').classList.remove('container');
+        document.getElementById('header-container').classList.add('container');
+    }
 })
