5f0d84023aa05cc1cac06e157d4e98920d1a5e31
unknown <a2mrken@gmail.com>
2021-05-04 17:52:05 +0700

    Udemy fix render tab-pane

--- Study/fe64_day10_Udemy/js/main.js
+++ Study/fe64_day10_Udemy/js/main.js
@@ -1,3 +1,10 @@
+$(document).ready(function () {
+  $('.tab-pane').css({ 'display': 'block' });
+  setTimeout(function () {
+    $('.tab-pane').removeAttr('style');
+
+  }, 500);
+});
 $(".owl-one").owlCarousel({
   dots: false,
   nav: true,
@@ -12,7 +19,7 @@ $(".owl-one").owlCarousel({
   },
 });
 $(".owl-two").owlCarousel({
-  loop:true,
+  loop: true,
   dots: false,
   nav: true,
   margin: 10,
@@ -38,7 +45,7 @@ $(".owl-three").owlCarousel({
     },
   },
 });
-// $(document).ready(function () {
+
 $('[data-toggle="popover"]').popover({
   animation: true,
   html: true,
@@ -49,7 +56,7 @@ $('[data-toggle="popover"]').popover({
   content:
     ' <div class="item__hover shadow border py-5 px-2 m-auto"><div class="container"><div class="row"><div class="col-md-12"><p>Last update: 07/2020</p><h3><a href="">The Complete Foundation Stock Trading Course</a></h3><p class="category"><span class="best-seller">best seller</span> in <a href="">Stock Trading</a> | Bussiness</p><ul class="details"><li><i class="fas fa-play-circle    "></i> 41 lectures</li><li><i class="fas fa-clock    "></i> 9.5 totals hours</li><li><i class="fas fa-filter    "></i> All levels</li><li><i class="fas fa-closed-captioning    "></i></li></ul><p>Learn To Trade The Stock Market by A Trading Firm CEO. Inc: Technical Analysis, Candlesticks, Stocks, Day Trading +++</p><ul class="infor"><li>Completely understand how the stock market works.</li><li>Know the process that a company goes through to become a Publicly Traded Company.</li><li>Know the history of the Stock Market and its Evolution.</li></ul><div><button class="btn btn-signup">Add to cart</button> <i class="fas fa-heart    "></i></div></div></div></div></div>',
 });
-// });
+
 
 $('[data-toggle="popover"]').on("show.bs.popover", function () {
   $('[data-toggle="popover"]').popover("hide");
